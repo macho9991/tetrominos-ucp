cbcc088d1d54f71fcf2b6caf57c44f3acb13c124
jeremorales22 <yoeljere377@gmail.com>
2026-08-31 17:20:33 -0300

    testdereloj

--- src/Reloj.ts
+++ src/Reloj.ts
@@ -1,9 +1,9 @@
-class Reloj {
+export class Reloj {
     private tiempoDeEspera: number;
 
     constructor(milisegundos: number) {
         // Guardamos cada cuánto tiempo queremos que el reloj haga "tic"
-        this.tiempoDeEspera = milisegundos; 
+        this.tiempoDeEspera = milisegundos;
     }
 
     // Esta función recibe otra función (una acción) y la repite sin parar
